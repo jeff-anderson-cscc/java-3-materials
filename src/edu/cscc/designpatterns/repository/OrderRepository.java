194acf3297e5c7b33409091ac09d72e90257fadf
Jeff Anderson <46822968+jeff-anderson-cscc@users.noreply.github.com>
2021-03-13 22:09:30 +0000

    Initialize in-class-exercises/threading-with-race-condition

---
+++ src/edu/cscc/designpatterns/repository/OrderRepository.java
@@ -0,0 +1,19 @@
+package edu.cscc.designpatterns.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface OrderRepository {
+    OrderRepository instance = new OrderRepositoryImpl();
+    static OrderRepository getInstance() {
+        return instance;
+    }
+
+    Order create(Order order);
+
+    Order read(UUID id);
+
+    Order update(Order order);
+
+    List<Order> readAll();
+}
