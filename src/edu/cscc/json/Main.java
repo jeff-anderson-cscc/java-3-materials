f601a90426108b249128525a2b98fe7af464a322
Jeff Anderson <46822968+jeff-anderson-cscc@users.noreply.github.com>
2021-03-13 22:09:17 +0000

    Initialize in-class-exercises/jpa-and-hibernate-relationships-starting-point

---
+++ src/edu/cscc/json/Main.java
@@ -0,0 +1,35 @@
+package edu.cscc.json;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        Type orderList = new TypeToken<ArrayList<Order>>(){}.getType();
+        try {
+            List<Order> orders = gson.fromJson(
+                    new FileReader("./src/edu/cscc/json/orders.json"),
+                    orderList
+            );
+            orders.forEach(System.out::println);
+
+            String output = gson.toJson(orders);
+            FileWriter fileWriter = new FileWriter("output.json");
+            fileWriter.write(output);
+            fileWriter.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
